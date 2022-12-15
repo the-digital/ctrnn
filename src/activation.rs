00c9d0a112eed4284bf81dcb1724d64b2220f58e
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-12-15 18:01:32 -0500

    Add inverse_sigmoid function

--- src/activation.rs
+++ src/activation.rs
@@ -5,6 +5,10 @@ pub fn sigmoid(x: f64) -> f64 {
     (1.0 + (-x).exp()).recip()
 }
 
+pub fn inverse_sigmoid(x: f64) -> f64 {
+    (x / (1.0 - x)).ln()
+}
+
 /// A curve that is `0` if the input is negative, and `x` if it is positive.
 pub fn relu(x: f64) -> f64 {
     x.max(0.0)
